8bab6b6bce1ed5699f3f1491ef5412f04a20ef0a
Lochinbek <lochinbek433@gmail.com>
2023-03-18 14:06:54 +0500

    initial commit

---
+++ src/test/java/uz/pdp/appproblemsolver/AppProblemSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.appproblemsolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProblemSolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
